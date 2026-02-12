4329af66a95b0735dd8627e18e431b46fc2ab5a0
Nana <98115326+winnrz@users.noreply.github.com>
2026-02-12 18:07:10 +0000

    Initialize Dart package scaffold

---
+++ example/modular_cart_core_example.dart
@@ -0,0 +1,6 @@
+import 'package:modular_cart_core/modular_cart_core.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/modular_cart_core.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/modular_cart_core_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
